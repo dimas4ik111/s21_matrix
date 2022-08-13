153678bc7a011dea1dec245a5abd1d4d78ed4001
Dimas4ik111 <osgreywi@student.21-school.ru>
2022-08-13 15:09:13 +0300

    l

--- src/library/s21_create_matrix.c
+++ src/library/s21_create_matrix.c
@@ -3,18 +3,23 @@
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
     int condition = SUCCESS;
 
-    if (rows <= 0 || columns <= 0 || !result->matrix)
+    if (rows <= 0 || columns <= 0)
         return FAILURE;
 
-    result->columns = columns;
-    result->rows = rows;
+    result->matrix = calloc(rows, sizeof(double*));
 
-    for (int i = 0; i < rows; i++) {
-        result->matrix[i] = calloc(columns, sizeof(double));
-        if (result->matrix[i] == NULL) {
-            condition = FAILURE;
-            break;
+    if (result->matrix != NULL) {
+        result->columns = columns;
+        result->rows = rows;
+        for (int i = 0; i < rows; i++) {
+            result->matrix[i] = calloc(columns, sizeof(double));
+            if (result->matrix[i] == NULL) {
+                condition = FAILURE;
+                break;
+            }
         }
+    } else {
+        condition = FAILURE;
     }
 
     return condition;
