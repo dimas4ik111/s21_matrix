52e7646edc6ad4d9d28f9a9fdc471f98d1f7fdc1
Dimas4ik111 <osgreywi@student.21-school.ru>
2022-08-14 14:29:10 +0300

    lol

--- src/library/s21_remove_matrix.c
+++
@@ -1,9 +0,0 @@
-#include "../s21_matrix.h"
-
-void s21_remove_matrix(matrix_t *a) {
-    if (a) {
-        for (int i = 0; i < a->rows; i++)
-            free(a->matrix[i]);
-        free(a->matrix);
-    }
-}
